a0a842b93057c2db5b309e381a9ffa69670b29bb
Ron Gierlach <rongierlach@gmail.com>
2017-04-16 07:07:40 -0500

    test(add default functionality tests for standard export):

--- test/standard.test.js
+++ test/standard.test.js
@@ -9,6 +9,48 @@ const argsToResult = (args, resultArr) =>
 
 describe('Standard export', () => {
   it(
+    'returns an empty array when no arguments are given',
+    () => { argsToResult([], []) }
+  )
+
+  it(
+    'returns an empty array when first and only argument is 0',
+    () => { argsToResult([0], []) }
+  )
+
+  it(
+    'returns an empty array when first argument is boolean',
+    () => {
+      argsToResult([true], [])
+      argsToResult([false], [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is an object',
+    () => {
+      argsToResult({}, [])
+      argsToResult({foo: 'bar'}, [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is an array',
+    () => {
+      argsToResult([[]], [])
+      argsToResult([1, 2, 3], [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is null or undefined',
+    () => {
+      argsToResult([null], [])
+      argsToResult([undefined], [])
+    }
+  )
+
+  it(
     'returns a num range when only first argument given',
     () => {
       argsToResult([4], [0, 1, 2, 3])
