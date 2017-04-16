4a223679d4376fdf9d0b4012af66e0db1194983c
Ron Gierlach <rongierlach@gmail.com>
2017-04-16 07:08:56 -0500

    test: scaffold tests for values export

---
+++ test/values.test.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+import range from '../src/values'
+
+const argsToResult = (args, resultArr) =>
+  expect(
+    range(...args))
+  .toEqual(
+    expect.arrayContaining(resultArr))
+
+describe('Values export', () => {
+  it(
+    'returns an empty array when no arguments are given',
+    () => { argsToResult([], []) }
+  )
+
+  it(
+    'returns an empty array when first and only argument is 0',
+    () => { argsToResult([0], []) }
+  )
+
+  it(
+    'returns an empty array when first argument is boolean',
+    () => {
+      argsToResult([true], [])
+      argsToResult([false], [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is an object',
+    () => {
+      argsToResult({}, [])
+      argsToResult({foo: 'bar'}, [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is an array',
+    () => {
+      argsToResult([[]], [])
+      argsToResult([1, 2, 3], [])
+    }
+  )
+
+  it(
+    'returns an empty array when first argument is null or undefined',
+    () => {
+      argsToResult([null], [])
+      argsToResult([undefined], [])
+    }
+  )
+})
