57a270caec52c13c60adb22089e9a63592fb3d85
Ron Gierlach <rongierlach@gmail.com>
2017-04-16 06:53:55 -0500

    feat: write logic for standard export

--- src/standard.js
+++ src/standard.js
@@ -1,12 +1,27 @@
-import {
-  compose,
-  handleDefaults
-} from './util'
+import { handleDefaults } from './util'
 
 const range = (start, end, step) => {
+  const result = []
 
+  if (step === undefined) {
+    if (end === undefined) {
+      end = start
+      start = 0
+    }
+    step = start < end ? 1 : -1
+  }
+
+  if (step === 0) {
+    for (let i = start; i < Math.abs(end - start); i++) {
+      result.push(start)
+    }
+  } else {
+    for (let val = start; (end - val) * step > 0; val += step) {
+      result.push(val)
+    }
+  }
+
+  return result
 }
 
-export default compose(
-  handleDefaults
-)(range)
+export default handleDefaults(range)
