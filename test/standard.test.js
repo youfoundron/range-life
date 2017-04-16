69cb21ca9fc04bacc68ae95629c06786b09b0843
Ron Gierlach <rongierlach@gmail.com>
2017-04-16 06:41:22 -0500

    test: write some tests for the standard export

---
+++ test/standard.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+import range from '../src/standard'
+
+const argsToResult = (args, resultArr) =>
+  expect(
+    range(...args))
+  .toEqual(
+    expect.arrayContaining(resultArr))
+
+describe('Standard export', () => {
+  it(
+    'returns a num range when only first argument given',
+    () => {
+      argsToResult([4], [0, 1, 2, 3])
+      argsToResult([-4], [0, -1, -2, -3])
+    }
+  )
+
+  it(
+    'returns a num range from start to end',
+    () => { argsToResult([1, 5], [1, 2, 3, 4]) }
+  )
+
+  it(
+    'returns an integer stepped range',
+    () => {
+      argsToResult([0, 20, 5], [0, 5, 10, 15])
+      argsToResult([0, -20, -5], [0, -5, -10, -15])
+    }
+  )
+
+  it(
+    'returns a decimal stepped range',
+    () => {
+      argsToResult([1, 4, 0.5], [1, 1.5, 2, 2.5, 3, 3.5])
+      argsToResult([1, -2, -0.5], [1, 0.5, 0, -0.5, -1.0, -1.5])
+    }
+  )
+
+  it(
+    'handles step of 0',
+    () => {
+      argsToResult([1, 4, 0], [1, 1, 1]),
+      argsToResult([1, -4, 0], [1, 1, 1, 1, 1])
+    }
+  )
+})
