bd787ac0811b8b8bc8ebe6fca136403d3a8cd453
Ron Gierlach <rongierlach@gmail.com>
2017-04-17 09:30:06 -0500

    style: standard tests conform to the same style

--- test/standard.test.js
+++ test/standard.test.js
@@ -5,40 +5,27 @@ import {argsToResultFactory} from './util'
 const argsToResult = argsToResultFactory(range)
 
 describe('Standard export', () => {
-  it(
-    'returns a num range when only first argument given',
-    () => {
-      argsToResult([4], [0, 1, 2, 3])
-      argsToResult([-4], [0, -1, -2, -3])
-    }
-  )
+  it('returns a num range when only first argument given', () => {
+    argsToResult([4], [0, 1, 2, 3])
+    argsToResult([-4], [0, -1, -2, -3])
+  })
 
-  it(
-    'returns a num range from start to end',
-    () => { argsToResult([1, 5], [1, 2, 3, 4]) }
-  )
+  it('returns a num range from start to end', () => {
+    argsToResult([1, 5], [1, 2, 3, 4])
+  })
 
-  it(
-    'returns an integer stepped range',
-    () => {
-      argsToResult([0, 20, 5], [0, 5, 10, 15])
-      argsToResult([0, -20, -5], [0, -5, -10, -15])
-    }
-  )
+  it('returns an integer stepped range', () => {
+    argsToResult([0, 20, 5], [0, 5, 10, 15])
+    argsToResult([0, -20, -5], [0, -5, -10, -15])
+  })
 
-  it(
-    'returns a decimal stepped range',
-    () => {
-      argsToResult([1, 4, 0.5], [1, 1.5, 2, 2.5, 3, 3.5])
-      argsToResult([1, -2, -0.5], [1, 0.5, 0, -0.5, -1.0, -1.5])
-    }
-  )
+  it('returns a decimal stepped range', () => {
+    argsToResult([1, 4, 0.5], [1, 1.5, 2, 2.5, 3, 3.5])
+    argsToResult([1, -2, -0.5], [1, 0.5, 0, -0.5, -1.0, -1.5])
+  })
 
-  it(
-    'handles step of 0',
-    () => {
-      argsToResult([1, 4, 0], [1, 1, 1]),
-      argsToResult([1, -4, 0], [1, 1, 1, 1, 1])
-    }
-  )
+  it('handles step of 0',() => {
+    argsToResult([1, 4, 0], [1, 1, 1]),
+    argsToResult([1, -4, 0], [1, 1, 1, 1, 1])
+  })
 })
