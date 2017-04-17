e38d099546b2a7c88b642b8b76ad8b5c7a5050e9
Ron Gierlach <rongierlach@gmail.com>
2017-04-17 15:58:59 -0500

    style: alpha tests conform to the same style

--- test/alpha.test.js
+++ test/alpha.test.js
@@ -7,31 +7,22 @@ const argsToResult = argsToResultFactory(range)
 const argsToError = argsToErrorFactory(range)
 
 describe('Alpha export', () => {
-  it(
-    'returns an inclusive array of lowercase letters',
-    () => {
-      argsToResult(['a', 'e'], ['a', 'b', 'c', 'd', 'e'])
-      argsToResult(['e', 'a'], ['e', 'd', 'c', 'b', 'a'])
-    }
-  )
+  it('returns an inclusive array of lowercase letters', () => {
+    argsToResult(['a', 'e'], ['a', 'b', 'c', 'd', 'e'])
+    argsToResult(['e', 'a'], ['e', 'd', 'c', 'b', 'a'])
+  })
 
-  it(
-    'returns an inclusive array of uppercase letters',
-    () => {
-      argsToResult(['A', 'E'], ['A', 'B', 'C', 'D', 'E'])
-      argsToResult(['E', 'A'], ['E', 'D', 'C', 'B', 'A'])
-    }
-  )
+  it('returns an inclusive array of uppercase letters', () => {
+    argsToResult(['A', 'E'], ['A', 'B', 'C', 'D', 'E'])
+    argsToResult(['E', 'A'], ['E', 'D', 'C', 'B', 'A'])
+  })
 
-  it(
-    'returns an inclusive array of integer stepped letters',
-    () => {
-      argsToResult(['a', 'e', 2], ['a', 'c', 'e'])
-      argsToResult(['e', 'a', -2], ['e', 'c', 'a'])
-      argsToResult(['A', 'E', 2], ['A', 'C', 'E'])
-      argsToResult(['E', 'A', -2], ['E', 'C', 'A'])
-    }
-  )
+  it('returns an inclusive array of integer stepped letters', () => {
+    argsToResult(['a', 'e', 2], ['a', 'c', 'e'])
+    argsToResult(['e', 'a', -2], ['e', 'c', 'a'])
+    argsToResult(['A', 'E', 2], ['A', 'C', 'E'])
+    argsToResult(['E', 'A', -2], ['E', 'C', 'A'])
+  })
 
   it('handles step of 0', () => {
     argsToResult(['a', 'c', 0], ['a', 'a', 'a'])
@@ -39,6 +30,6 @@ describe('Alpha export', () => {
   })
 
   it('throws if step is not an integer', () => {
-    expect(range.bind(null, 'a', 'e', 0.5)).toThrowError(errors.StepIsNotInteger)
+    argsToError(['a', 'e', 0.5], errors.StepIsNotInteger)
   })
 })
