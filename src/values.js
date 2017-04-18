855c955e38191f8bdf27cc310e8e860ec9d2f9c8
Ron Gierlach <rongierlach@gmail.com>
2017-04-17 19:22:15 -0500

    feat: add functionality for values export

--- src/values.js
+++ src/values.js
@@ -5,19 +5,45 @@ import {
   throwIfStepIsNotInteger
 } from './util'
 
-const normalizeValues = transformFunc(
-  (next, ...args) =>
-    Array.isArray(args[2])
-      ? next(args[0], args[1], 0, args[2])
-      : next(...args)
+const normalizeParams = transformFunc(
+  (next, start, end, step, values) => {
+    if (Array.isArray(step)) {
+      values = step
+      step = undefined
+    }
+    if (step === undefined) {
+      if (end === undefined) {
+        end = start
+        start = 0
+      }
+      step = start < end ? 1 : -1
+    }
+    return next(start, end, step, values)
+  }
 )
 
-const range = (start, end, step, values) => {
+const range = (startIndex, endIndex, step, values) => {
+  const result = []
 
+  if (step === 0) {
+    for (
+      let currIndex = startIndex;
+      currIndex < Math.abs(startIndex - endIndex);
+      currIndex++
+    ) result.push(values[currIndex])
+  } else {
+    for (
+      let currIndex = startIndex;
+      (endIndex - currIndex) * step;
+      currIndex += step
+    ) result.push(values[currIndex])
+  }
+
+  return result
 }
 
 export default compose(
-  throwIfStepIsNotInteger,
-  normalizeValues,
-  handleDefaults
+  normalizeParams,
+  handleDefaults,
+  throwIfStepIsNotInteger
 )(range)
